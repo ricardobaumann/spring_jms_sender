42cb8598cd41fe6aedf54163032b1189b207a3f6
ricardobaumann <ricardo.luis.baumann@gmail.com>
2017-01-12 10:43:50 +0100

    Project creation

---
+++ src/main/java/jms_sending_example/JmsConfiguration.java
@@ -0,0 +1,43 @@
+package jms_sending_example;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+/**
+ * Created by ricardobaumann on 11/01/17.
+ */
+@Configuration
+public class JmsConfiguration {
+
+    private static final String JMS_BROKER_URL = "tcp://localhost:61616";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory(JMS_BROKER_URL);
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+        return jmsTemplate;
+    }
+
+
+}
---
+++ src/main/java/jms_sending_example/JmsSendingExampleApplication.java
@@ -0,0 +1,16 @@
+package jms_sending_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by ricardobaumann on 11/01/17.
+ */
+@SpringBootApplication
+public class JmsSendingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSendingExampleApplication.class,args);
+
+    }
+
+}
